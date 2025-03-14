7fa676a723c00b6b920089d842125fc70e4ac428
EAKOUASSI <eak.kouassi@gmail.com>
2025-03-14 17:43:48 +0000

    first commit

---
+++ Algo_Tri.js
@@ -0,0 +1,20 @@
+
+function insertionSort(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        let key = arr[i];
+        let j = i - 1;
+        
+        
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+    return arr;
+}
+
+let arr = [5, 6, 77, 88, 99];
+console.log("Tableau initial : " + arr);
+let sortdArr = insertionSort(arr);
+console.log("Tableau trié : " + sortdArr);

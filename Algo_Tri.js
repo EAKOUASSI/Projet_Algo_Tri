c674e07e15409b86367a437fd8f11656e135afb3
EAKOUASSI <eak.kouassi@gmail.com>
2025-03-14 17:51:39 +0000

    Commentaires ajoutés

--- Algo_Tri.js
+++ Algo_Tri.js
@@ -1,10 +1,11 @@
-
+// Choix de l'élément à insérer
 function insertionSort(arr) {
     for (let i = 1; i < arr.length; i++) {
         let key = arr[i];
         let j = i - 1;
-        
-        
+
+// Déplacez les éléments, qui sont plus grands que key,
+       
         while (j >= 0 && arr[j] > key) {
             arr[j + 1] = arr[j];
             j = j - 1;
